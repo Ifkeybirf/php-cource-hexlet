da3ccebc77f25a87e7117627cd117ec064dfba56
Nikolai Gordeev <ngordeev@plesk.com>
2021-12-18 20:27:44 +0700

    exercise #8: FizzBuzz

---
+++ practice/fizzBuzz/fizzBuzz.php
@@ -0,0 +1,42 @@
+<?php
+
+function isDivision(int $divided, int $divider): bool{
+    $a = $divided / $divider;
+    $b = intdiv($divided, $divider);
+    return ($a ===$b) ? true : false ;
+}
+//var_dump(isDivision(5,2) ) ;
+function fizzBuzz(int $begin, int $end) {
+    if ($begin > $end)
+    {
+        return ' ';
+    }
+
+    define( 'fizz' , 3);
+    define ('buzz' , 2);
+    
+    $qq = $begin;
+    while ($qq <= $end)
+    {
+        $resFizz = isDivision($qq, fizz);
+        $resBuzz = isDivision($qq, buzz);
+        $resFizzBuzz = ( $resFizz && $resBuzz) ;
+        print "(iter $qq) ";
+        if ($resFizz && $resBuzz)
+            {
+                print 'FizzBuzz ';
+            }elseif ($resFizz)
+            {
+                print 'Fizz ';
+            }elseif ($resBuzz)
+            {
+                print 'Buzz ';
+            }elseif (!$resFizz && !$resBuzz && !$resFizzBuzz)
+                {
+                    print "$qq ";
+                }
+    $qq += 1 ;
+    }
+}
+
+fizzBuzz(9, 15);
