d4fec49c6ddc8723e976f9543648b6dcb5ab5de6
Nikolai Gordeev <ngordeev@plesk.com>
2021-12-20 17:35:45 +0700

    satisfy PSR

--- practice/add_digits/addDigits.php
+++ practice/add_digits/addDigits.php
@@ -13,26 +13,28 @@ function addDigits( $userInput)
             $lng = strlen($str);
 //            var_dump($lng);echo "<-lng\n";
 
-            $res = 0; $qq = 0;
-//            for ($qq = 0; $qq < $lng; $qq++) {
-            while ($qq < $lng){
-//                print "qq $qq\n";
-//                var_dump('str[$qq] ' . $str[$qq]);
+            $res = 0;
+            $qq = 0;
+//          for ($qq = 0; $qq < $lng; $qq++) {
+            while ($qq < $lng) {
+//              print "qq $qq\n";
+//              var_dump('str[$qq] ' . $str[$qq]);
                 $res = $res + $str[$qq];
-//                var_dump('res: ' . $res . PHP_EOL);
-                $qq += 1 ;
+//              var_dump('res: ' . $res . PHP_EOL);
+                $qq++;
             }
-            if ($res >9 )
+            if ($res > 9)
             {
                 addDigits($res);
             } else
-                print 'answer is ' . $res . PHP_EOL; return $res;
+                print 'answer is ' . $res . PHP_EOL;
 
+            return $res;
         }
     }
 }
 
-$ask = 221513 ;
+$ask = 221513;
 //$ask = 38;
 print_r($ask . '<= has been entered' . PHP_EOL);
 addDigits($ask);
