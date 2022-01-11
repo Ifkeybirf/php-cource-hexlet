23f353528b3ee6b3275906c6e9fe89e78d50d7a0
Nikolai Gordeev <ngordeev@plesk.com>
2022-01-11 16:19:01 +0700

    fixUp! +separate checking for input zero value

--- practice/isPerfect/isPerfect.php
+++ practice/isPerfect/isPerfect.php
@@ -2,13 +2,16 @@
 
 function isPerfect(int $a): bool
 {
+    if ($a === 0) {
+        return false;
+    }
     $dividers = 0;
     for ($i = 1; $i < $a; $i++) {
-        if ($a % $i == 0) {
+        if ($a % $i === 0) {
             $dividers += $i;
         }
     }
-    return $dividers == $a;
+    return $dividers === $a;
 }
 
 var_dump(isPerfect(28));
