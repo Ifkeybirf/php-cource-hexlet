e828116da40ab58543bc959abfe0b8ea4be472d1
Ifkeybirf <ifkeybirf@hotmail.com>
2022-01-18 17:29:41 +0700

    refactor ex7 to classes

---
+++ src/ex7/HappyTicket.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PHPCourse\ex7;
+
+class HappyTicket
+{
+
+    public function randomTicketNumber(int $cnt)
+    {
+        if ($cnt % 2 !== 0) {
+            print_r('the number of digits must be even' . PHP_EOL);
+            return 0;
+        }
+
+        $txt = '';
+        for ($qq = 1; $qq <= $cnt; $qq++) {
+            $tmp = random_int(0, 9);
+            $txt .= $tmp;
+        }
+        return $txt;
+    }
+
+    public function isHappy(string $ticketNumber): bool
+    {
+        if (!is_numeric($ticketNumber)) {
+            return false;
+        }
+        $ticketLen = strlen($ticketNumber);
+        /*
+         * even ticketLen used only twice in code below
+         * I decided that it is better than count strlen 3 times or more with code below
+         *
+         * additionally I'm not sure how to better collapse two 'if' below
+         */
+        if ($ticketLen % 2 !== 0) {
+            return false;
+        }
+        if ($ticketLen < 2) {
+            return false;
+        }
+
+        $lSum = $rSum = 0;
+        for ($qq = 0; $qq < $ticketLen / 2; $qq++) {
+            $lSum += (int)$ticketNumber[$qq];
+            $rSum += (int)$ticketNumber[$qq + $ticketLen / 2];
+        }
+
+        /*
+         * I did not use construction below
+         * return $lSum === $rSum && $halfNum > 1;
+         * because in this case we'll continue try to count lSum & rSum even halfNum < 1
+         * however, this is the reason to return that ticket is unhappy without additional calculations.
+         */
+        return $lSum === $rSum;
+    }
+
+}
