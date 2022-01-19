cdee3dc7bef096d9e0caf36c3e80be7046920636
Ifkeybirf <ifkeybirf@hotmail.com>
2022-01-19 17:32:33 +0700

    ex1.binarySum: handle incorrect input with exception; make positive and negative tests (that catch exceptions)

--- src/ex1/BinarySum.php
+++ src/ex1/BinarySum.php
@@ -1,6 +1,12 @@
 <?php
 namespace PHPCourse\ex1;
 
+assert_options(ASSERT_ACTIVE, 1);
+assert_options(ASSERT_WARNING, 0);
+
+use mysql_xdevapi\Exception;
+use function Webmozart\Assert\Tests\StaticAnalysis\string;
+
 class BinarySum
 {
     public function binarySum(string $addend1, string $addend2): string
@@ -8,11 +14,9 @@ class BinarySum
         $pattern = '/^[0-1]+$/';
 
         if (!preg_match($pattern, $addend1)) {
-            return 'entered value is incorrect: summand';
-        }
-
-        if (!preg_match($pattern, $addend2)) {
-            return 'entered value is incorrect: addend';
+            throw new \InvalidArgumentException('addend1 should contain only 0 and 1');
+        } elseif (!preg_match($pattern, $addend2)) {
+            throw new \InvalidArgumentException('addend2 should contain only 0 and 1');
         }
 
         $sumDec = bindec($addend1) + bindec($addend2);
@@ -23,5 +27,5 @@ class BinarySum
  * $a = '1101';
  * $b = '101';
  *
- * print_r("$a + $b = " . binarySum($a, $b) . PHP_EOL);
+ * print_r("$a + $b = " . (new BinarySum())->binarySum($a, $b)  . PHP_EOL);
  */
--- tests/ex1/BinarySumTest.php
+++ tests/ex1/BinarySumTest.php
@@ -3,11 +3,46 @@ use PHPUnit\Framework\TestCase;
 
 class BinarySumTest extends TestCase
 {
-    public function testBinarySum(): void
+    /**
+     * @dataProvider binarySumProvider
+     * @return array
+     */
+    public function testBinarySum($exp, $num1, $num2): void
     {
         $check1 = new \PHPCourse\ex1\BinarySum();
-        $actual = $check1->binarySum('111', 100);
-        $expected = '1011';
-        self::assertEquals($expected, $actual);
+        $actual = $check1->binarySum($num1, $num2);
+        self::assertEquals($exp, $actual);
+    }
+
+    public function binarySumProvider(): array
+    {
+        return [
+            ['10101','10000','101'],
+            ['101','1','100'],
+            ['1','0','1'],
+            ['0','0','0'],
+        ];
+    }
+
+    /**
+     * @dataProvider binarySumWrongProvider
+     * @expectedException \InvalidArgumentException
+     */
+    public function testBinarySumWrong($num1,$num2): void
+    {
+        $check = new \PHPCourse\ex1\BinarySum();
+        $this::getExpectedExceptionMessage('addend1 should contain only 0 and 1') || self::getExpectedExceptionMessage('addend2 should contain only 0 and 1');
+        $this->expectException(\InvalidArgumentException::class);
+        $check->binarySum($num1, $num2);
+    }
+
+    public function binarySumWrongProvider(): array
+    {
+        return [
+            ['12','100'],
+            ['a1','1'],
+            ['0','a'],
+            ['1','2'],
+        ];
     }
 }
