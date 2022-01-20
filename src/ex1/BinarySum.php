c50d9c424827071d680b223b228b3acb2419c038
Ifkeybirf <ifkeybirf@hotmail.com>
2022-01-20 13:44:25 +0700

    reorganize binarySum to class BinarySum; refactor; cover by test

---
+++ src/ex1/BinarySum.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PHPCourse\ex1;
+
+class BinarySum
+{
+    public function binarySum(string $addend1, string $addend2): string
+    {
+        $pattern = '/^[0-1]+$/';
+
+        if (!preg_match($pattern, $addend1)) {
+            throw new \InvalidArgumentException('addend1 should contain only 0 and 1');
+        }
+
+        if (!preg_match($pattern, $addend2)) {
+            throw new \InvalidArgumentException('addend2 should contain only 0 and 1');
+        }
+        return decbin(bindec($addend1) + bindec($addend2));
+    }
+}
